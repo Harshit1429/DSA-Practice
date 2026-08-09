461c2e6ad508774b0c637959f216303d5c0d3946
HARSHIT SHARMA <160218725+Harshit1429@users.noreply.github.com>
2026-08-09 19:27:53 +0530

    Time: 155 ms (33.98%), Space: 239.8 MB (65.43%) - LeetHub

---
+++ 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long weightedSum(vector<int>& parent, vector<int>& nums) {
+        int n = parent.size();
+
+        vector<vector<int>> children(n);
+
+        for (int i = 1; i < n; i++) {
+            children[parent[i]].push_back(i);
+        }
+
+        vector<int> depth(n);
+
+        queue<int> q;
+        q.push(0);
+
+        depth[0] = 1;
+        int height = 1;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int child : children[node]) {
+                depth[child] = depth[node] + 1;
+                height = max(height, depth[child]);
+
+                q.push(child);
+            }
+        }
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans += 1LL * nums[i] * (height - depth[i] + 1);
+        }
+
+        return ans;
+    }
+};

040075d748d3f10b23c5a42d1ad9caa08b6234cc
HARSHIT SHARMA <160218725+Harshit1429@users.noreply.github.com>
2026-09-04 20:07:53 +0530

    Time: 4 ms (13.74%), Space: 31.4 MB (36.87%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> maxx(n), minn(n);
+        maxx[0] = nums[0];
+        minn[n - 1] = nums[n - 1];
 
-         int Prevmaxi = nums[0];
-        for(int i = 0 ; i < nums.size() ; i++){
-            Prevmaxi = max(Prevmaxi , nums[i]);
-            int mini = findmini(i , nums);
-            if(abs(Prevmaxi - mini) <= k){
+        for (int i = 1; i < n; i++) {
+            maxx[i] = max(maxx[i - 1], nums[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            minn[i] = min(minn[i + 1], nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (maxx[i] - minn[i] <= k)
                 return i;
-            }
         }
+
         return -1;
-    }
-    int findmini(int i ,vector<int>& nums ){
-        int mini = INT_MAX;
-        for(int num  = i ; num < nums.size() ; num++){
-             mini = min(mini , nums[num]);
-        }
-        return mini;
     }
 };
